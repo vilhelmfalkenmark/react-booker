473ab74de98ac49c859047b4a0d67ef561f6e923
vilhelmfalkenmark <vilhelmfalkenmark@gmail.com>
2016-05-25 18:54:45 +0200

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import myApp from "./components/myApp.js";
+require("../scss/stylesheet.scss");
+
+ReactDOM.render(<myApp/>, document.getElementById('app') );
